05d2956966622a370a6b2ad09dd4e86cdf5eec32
Ihor <ihor.rome@gmail.com>
2024-12-03 19:10:06 +0200

    add column entity annotations

--- src/main/java/core/basesyntax/bookstore/model/Book.java
+++ src/main/java/core/basesyntax/bookstore/model/Book.java
@@ -1,5 +1,6 @@
 package core.basesyntax.bookstore.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,10 +21,18 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String title;
+
+    @Column(nullable = false)
     private String author;
+
+    @Column(unique = true, nullable = false)
     private String isbn;
+
+    @Column(nullable = false)
     private BigDecimal price;
+
     private String description;
     private String coverImage;
 
