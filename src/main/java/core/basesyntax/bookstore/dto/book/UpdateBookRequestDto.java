532778c869a31035763d46bdecb718c3677a5898
Ihor <ihor.rome@gmail.com>
2024-12-06 15:47:18 +0200

    add dto validation and global exception handler

--- src/main/java/core/basesyntax/bookstore/dto/book/UpdateBookRequestDto.java
+++ src/main/java/core/basesyntax/bookstore/dto/book/UpdateBookRequestDto.java
@@ -1,11 +1,12 @@
 package core.basesyntax.bookstore.dto.book;
 
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
-public record UpdateBookRequestDto(String title,
-                                   String author,
-                                   String isbn,
-                                   BigDecimal price,
+public record UpdateBookRequestDto(@NotNull String title,
+                                   @NotNull String author,
+                                   @NotNull String isbn,
+                                   @NotNull BigDecimal price,
                                    String description,
                                    String coverImage) {
 }
