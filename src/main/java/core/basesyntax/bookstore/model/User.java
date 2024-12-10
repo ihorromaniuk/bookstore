c4242e8208b41c7da47f668526f0e38a85deb7b9
Ihor <ihor.rome@gmail.com>
2024-12-10 10:46:47 +0200

    add join fetch instead of fetch type eager

--- src/main/java/core/basesyntax/bookstore/model/User.java
+++ src/main/java/core/basesyntax/bookstore/model/User.java
@@ -2,7 +2,6 @@ package core.basesyntax.bookstore.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -40,7 +39,7 @@ public class User implements UserDetails {
 
     private String shippingAddress;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
--- src/main/java/core/basesyntax/bookstore/repository/user/UserRepository.java
+++ src/main/java/core/basesyntax/bookstore/repository/user/UserRepository.java
@@ -3,9 +3,11 @@ package core.basesyntax.bookstore.repository.user;
 import core.basesyntax.bookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
 
+    @Query("from User u left join fetch u.roles where u.email = :email")
     Optional<User> findByEmail(String email);
 }
