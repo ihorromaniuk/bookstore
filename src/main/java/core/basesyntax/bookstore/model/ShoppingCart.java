a0c4c57631336c9ded573b9c21e5c8d44e209831
Ihor <ihor.rome@gmail.com>
2024-12-21 18:01:47 +0200

    resolving merge conflicts

--- src/main/java/core/basesyntax/bookstore/model/ShoppingCart.java
+++ src/main/java/core/basesyntax/bookstore/model/ShoppingCart.java
@@ -3,6 +3,7 @@ package core.basesyntax.bookstore.model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.MapsId;
